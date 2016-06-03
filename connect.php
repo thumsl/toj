6edf24ace45d50ce39552669609d43ca57ec5fd1
Luan Cristian Thums <luancthums@gmail.com>
2016-06-02 23:21:53 -0300

    stopped printing database information

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
 	function connectDB($host, $port, $user, $passwd, $dbname) {
 		$constring = "host=".$host." port=".$port." user=".$user." password=".$passwd." dbname=".$dbname;
-		echo $constring."<br>";
+//		echo $constring."<br>";
 		return pg_connect($constring);
 	}
 
 	$con = 	connectDB("localhost", "5432", "postgres", "QpT{2RsS", "judge");
 	if (!$con)
 		echo "Connection to the database failed";
-	else
-		echo "Successfully connected to the database (" . pg_dbname($con) . ")";
+//	else
+//		echo "Successfully connected to the database (" . pg_dbname($con) . ")";
 
 ?>
